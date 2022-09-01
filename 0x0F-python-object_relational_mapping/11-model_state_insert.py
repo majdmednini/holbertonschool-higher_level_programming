e568861fd8794457a0a2f0c5522a32759dc53c80
threadsv1 <majdimed3@gmail.com>
2022-09-01 20:05:38 +0100

    updated

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+add the state object Louisiana to a db
+"""
+
+import sqlalchemy
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_enginea
+
+
+if __name__ == "__main__":
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    louis = State(name='Louisiana')from sqlalchemy.orm import sessionmaker
+    session.add(louis)
+    state = session.query(State).filter_by(name='Louisiana').first()
+    print(str(state.id))
+    session.commit()
