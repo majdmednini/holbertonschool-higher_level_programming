e45c9318a2c9f91c3221955a99f3c2a572d6d4d5
threadsv1 <majdimed3@gmail.com>
2022-09-13 18:49:04 +0100

    updated

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-const r = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const request = require('request');
+const starWarsUri = 'https://swapi-api.hbtn.io/api/films/'.concat(process.argv[2]);
 
-r.get(url, (err, res, body) => {
-  if (err) console.log(err);
-  else console.log(JSON.parse(body).title);
+request(starWarsUri, function (_err, _res, body) {
+	  body = JSON.parse(body);
+	  console.log(body.title);
 });
+
