4a474147488c0ae6e9effac40d685e96a7fff56e
threadsv1 <majdimed3@gmail.com>
2022-09-13 18:54:27 +0100

    fixed

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const r = require('request');
+const request = require('request');
 const fs = require('fs');
 
-r.get(process.argv[2], (err, res, body) => {
+request.get(process.argv[2], (err, res, body) => {
   if (err) console.log(err);
   else {
     fs.writeFile(process.argv[3], body, 'utf8', (err) => {
