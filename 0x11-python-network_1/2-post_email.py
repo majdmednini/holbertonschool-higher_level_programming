03215c2b512ccd155381dd8aa7c794a491edbb8c
threadsv1 <majdimed3@gmail.com>
2022-10-26 16:12:27 +0100

    pep8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter
+takes in a URL and an email, sends a POST request to the passed URL with the email
 """
 if __name__ == "__main__":
     import urllib.parse as parse
