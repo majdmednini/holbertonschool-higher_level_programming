09e300955894de86ffbce6de2d3486e9f3c892da
threadsv1 <majdimed3@gmail.com>
2022-10-26 11:27:25 +0100

    pep8

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-t takes in a URL, sends a request to the URL and displays the value of the X-Request-Id
+t takes in a URL, sends a request to the URL and displays the value
 """
 if __name__ == "__main__":
     import urllib.request as request
